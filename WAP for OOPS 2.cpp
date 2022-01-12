815cc390d17081d62d166ed63b44861a9beb238e
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-12 19:11:56 +0530

    Create WAP for OOPS 2.cpp

---
+++ WAP for OOPS 2.cpp
@@ -0,0 +1,16 @@
+//WAP for OOPS 2
+#include<iostream>
+using namespace std;
+class rectangle{
+	public:
+		rectangle(){   //constructor
+		int l,b,area;
+		cout<<"Enter lenght and breadth of a rectangle\n";
+		cin>>l>>b;
+		cout<<"Area = "<<l*b;
+		}
+};
+int main(){
+	rectangle obj;   //instance of a class
+	return 0;
+}
