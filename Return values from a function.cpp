78a8dd86c5a72cd7678509b6b50fd85da3254f54
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-25 10:14:06 +0530

    Create Return values from a function.cpp

---
+++ Return values from a function.cpp
@@ -0,0 +1,13 @@
+//WAP to return values from a function
+#include<iostream>
+using namespace std;
+
+int area(int l,int b){
+	return l*b;
+}
+
+int main(){
+	int a = area(4,2);
+	cout<<a;
+	return 0;
+}
