4624c8552456fafd9fd53d74edc0d191ae8499b0
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-25 10:35:37 +0530

    Create Inline function.cpp

---
+++ Inline function.cpp
@@ -0,0 +1,10 @@
+//Inline function
+#include<iostream>
+using namespace std;
+inline int add(int a,int b){
+	return (a+b);
+}
+int main(){
+	cout<<"Addition of a and b is "<< add (2,3);
+	return 0;
+}
